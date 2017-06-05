ea80e0017ff4fa27b54c51aac74b739554b6e457
Ronan Luiz Araujo Eustaquio <ronan@ciandt.com>
2017-06-05 20:51:57 -0300

    Implementação da #App 10 - Lista de Tarefas
    - Armazenamento / leitura de dados a partir do UserDefaults
    - Utilização de Bar Button Item (Add)
    - Evento de exclusão de item a partir do table view

--- Lista de Tarefas/Lista de Tarefas/TarefaBusiness.swift
+++ Lista de Tarefas/Lista de Tarefas/TarefaBusiness.swift
@@ -7,3 +7,36 @@
 //
 
 import Foundation
+
+class TarefaBusiness {
+    
+    let chaveListaTarefas = "chaveListaTarefas"
+    var tarefas: [String] = []
+    
+    func salvar(forTarefa tarefa: String){
+        tarefas = listar()
+        
+        tarefas.append(tarefa)
+        
+        UserDefaults.standard.set(tarefas,  forKey: chaveListaTarefas)
+    }
+    
+    func listar() -> [String] {
+        
+        let dados = UserDefaults.standard.object(forKey: chaveListaTarefas)
+        
+        if dados is [String] {
+            return dados as! [String]
+        } else {
+            return []
+        }
+    }
+    
+    func excluir(indice: Int){
+        tarefas = listar()
+        
+        tarefas.remove(at: indice)
+        
+        UserDefaults.standard.set(tarefas,  forKey: chaveListaTarefas)
+    }
+}
--- Lista de Tarefas/Lista de Tarefas/ViewController.swift
+++ Lista de Tarefas/Lista de Tarefas/ViewController.swift
@@ -8,18 +8,57 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    var tarefas: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        atualizarTarefas()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        atualizarTarefas()
+        tableView.reloadData()
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tarefas.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let celula = tableView.dequeueReusableCell(withIdentifier: "celulaTabela", for: indexPath)
+        celula.textLabel?.text = tarefas[indexPath.row]
+        
+        return celula
+    }
+    
+    /**
+     Evento que permite definir ação de exclusão no table view
+     */
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == UITableViewCellEditingStyle.delete {
+            let tarefaBusiness = TarefaBusiness()
+            tarefaBusiness.excluir(indice: indexPath.row)
+            
+            atualizarTarefas()
+            tableView.reloadData()
+        }
+    }
+    
+    func atualizarTarefas() {
+        let tarefaBusiness = TarefaBusiness()
+        tarefas = tarefaBusiness.listar()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
