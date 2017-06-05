ec152983423887164c7621ce6d166fb528f04d40
Ronan Luiz Araujo Eustaquio <ronan@ciandt.com>
2017-06-05 20:12:20 -0300

    Implementação da #App 10 - Lista de Tarefas
    - Armazenamento / leitura de dados a partir do UserDefaults
    - Utilização de Bar Button Item (Add)
    - Evento de exclusão de item a partir do table view

---
+++ Lista de Tarefas/Lista de Tarefas/TarefaBusiness.swift
@@ -0,0 +1,9 @@
+//
+//  TarefaBusiness.swift
+//  Lista de Tarefas
+//
+//  Created by Ronan Luiz Araujo Eustaquio on 02/06/17.
+//  Copyright © 2017 Rlz System. All rights reserved.
+//
+
+import Foundation
