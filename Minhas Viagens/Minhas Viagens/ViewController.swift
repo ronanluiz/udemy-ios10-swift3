74790f708657f747dfc09414d13e2c2b5243e388
Ronan Luiz Araujo Eustaquio <ronan@ciandt.com>
2017-06-22 21:57:27 -0300

    Criação da #App12 - Minhas Viagens
    - Reconhecimento de gesto a partir do mapa (UILongPressGestureRecognizer)
    - Recuperação de coordenadas a partir do ponto selecionado
    - Recuperação de endereço a partir de coordenadas
    - Inclusão de ponto no mapa com dados do endereço

--- Minhas Viagens/Minhas Viagens/ViewController.swift
+++ Minhas Viagens/Minhas Viagens/ViewController.swift
@@ -18,6 +18,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         setupLocationManager()
+        setupReconhecedorGestos()
         
     }
 
@@ -29,6 +30,53 @@ class ViewController: UIViewController {
 }
 
 extension ViewController: MKMapViewDelegate {
+    
+    func setupReconhecedorGestos(){
+        let reconhecdorGestos = UILongPressGestureRecognizer(target: self, action: #selector(ViewController.marcarLocalizacao(gesture:)))
+        reconhecdorGestos.minimumPressDuration = 2
+        
+        mapa.addGestureRecognizer(reconhecdorGestos)
+        
+    }
+    
+    func marcarLocalizacao(gesture: UIGestureRecognizer){
+        if gesture.state == .began {
+            
+            // recupera as coordenadas do ponto selecionado
+            let pontoSelecionado = gesture.location(in: self.mapa)
+            let coordenadas = mapa.convert(pontoSelecionado, toCoordinateFrom: self.mapa)
+            let localizacao = CLLocation(latitude: coordenadas.latitude, longitude: coordenadas.longitude)
+            
+            // recuperar endereço a partir das coordenadas
+            var localCompleto = "Endereço não encontrado"
+            CLGeocoder().reverseGeocodeLocation(localizacao, completionHandler: { (local, erro) in
+                if erro == nil {
+                    
+                    if let dadosLocal = local?.first {
+                        if let nomeLocal = dadosLocal.name {
+                            localCompleto = nomeLocal
+                        } else if let endereco = dadosLocal.thoroughfare {
+                            localCompleto = endereco
+                        }
+                    }
+                    
+                    // exibe a anotação com dados do endereço
+                    let anotacao = MKPointAnnotation()
+                    
+                    anotacao.coordinate.latitude = coordenadas.latitude
+                    anotacao.coordinate.longitude = coordenadas.longitude
+                    anotacao.title = localCompleto
+                    
+                    self.mapa.addAnnotation(anotacao)
+                    
+                } else {
+                  print(erro!)
+                }
+            })
+            
+            
+        }
+    }
     
 }
 
