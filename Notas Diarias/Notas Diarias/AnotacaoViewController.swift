612cd11213bfa568fa11166d4ca666e46a0b2a6b
Ronan Luiz Araujo Eustaquio <ronan@ciandt.com>
2017-07-17 23:24:21 -0300

    #App13 - Notas Diárias
    - Criação da interface com navigation controller e table view

---
+++ Notas Diarias/Notas Diarias/AnotacaoViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Notas Diarias
+//
+//  Created by Ronan Luiz Araujo Eustaquio on 17/07/17.
+//  Copyright © 2017 Rlz System. All rights reserved.
+//
+
+import UIKit
+
+class AnotacaoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
