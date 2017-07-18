836a55aa4ea2e92786595ac7c1fd1b9bbc44ae26
Ronan Luiz Araujo Eustaquio <ronan@ciandt.com>
2017-07-17 23:57:05 -0300

    #App13 - Notas Diárias
    - Salvando anotação em uma entidade via Core Data

--- Notas Diarias/Notas Diarias/AnotacaoViewController.swift
+++ Notas Diarias/Notas Diarias/AnotacaoViewController.swift
@@ -7,14 +7,47 @@
 //
 
 import UIKit
+import CoreData
 
 class AnotacaoViewController: UIViewController {
 
+    var contextManager: NSManagedObjectContext!
+    
+    @IBOutlet weak var texto: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+        // inicializando contexto do core data
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        contextManager = appDelegate.persistentContainer.viewContext
+        
+        // força abrir o teclado no campo texto
+        texto.becomeFirstResponder()
+        texto.text = ""
     }
 
+    @IBAction func salvarAnotacao(_ sender: Any) {
+        salvar()
+        
+        //voltar para página inicial
+        navigationController?.popToRootViewController(animated: true)
+    }
+    
+    fileprivate func salvar() {
+        let anotacao = NSEntityDescription.insertNewObject(forEntityName: "Anotacao", into: contextManager)
+        
+        anotacao.setValue(texto.text, forKey: "texto")
+        anotacao.setValue(Date(), forKey: "data")
+        
+        do {
+            try contextManager.save()
+            print("Anotação salva com sucesso.")
+        } catch let error as NSError {
+            print("Erro ao salvar anotação: \(error.description)")
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
