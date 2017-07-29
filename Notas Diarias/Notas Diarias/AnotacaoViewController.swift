e38b03c69be6ba1008c42dcb7521f73ae2219a8f
Ronan Luiz Araujo Eustaquio <ronan@ciandt.com>
2017-07-28 23:30:00 -0300

    #App13 - Notas Diárias
    - Exibição do registro selecionado na table view pela segue
    - Atualização do registro utilizando CoreData

--- Notas Diarias/Notas Diarias/AnotacaoViewController.swift
+++ Notas Diarias/Notas Diarias/AnotacaoViewController.swift
@@ -11,48 +11,67 @@ import CoreData
 
 class AnotacaoViewController: UIViewController {
 
-    var contextManager: NSManagedObjectContext!
-    
+    var gerenciadorContexto: NSManagedObjectContext!
+    var anotacao: NSManagedObject!
     @IBOutlet weak var texto: UITextView!
+    
+    var temAnotacaoSelecionada: Bool {
+        return anotacao != nil
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
     
         // inicializando contexto do core data
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
-        contextManager = appDelegate.persistentContainer.viewContext
+        gerenciadorContexto = appDelegate.persistentContainer.viewContext
         
         // força abrir o teclado no campo texto
         texto.becomeFirstResponder()
-        texto.text = ""
+        
+        if temAnotacaoSelecionada {
+            texto.text = anotacao.value(forKey: "texto") as? String
+        } else {
+            texto.text = ""
+        }
     }
 
     @IBAction func salvarAnotacao(_ sender: Any) {
-        salvar()
+        if temAnotacaoSelecionada {
+            atualizar()
+        } else {
+            salvar()
+        }
         
         //voltar para página inicial
         navigationController?.popToRootViewController(animated: true)
     }
     
     fileprivate func salvar() {
-        let anotacao = NSEntityDescription.insertNewObject(forEntityName: "Anotacao", into: contextManager)
+        let anotacao = NSEntityDescription.insertNewObject(forEntityName: "Anotacao", into: gerenciadorContexto)
         
         anotacao.setValue(texto.text, forKey: "texto")
         anotacao.setValue(Date(), forKey: "data")
         
         do {
-            try contextManager.save()
+            try gerenciadorContexto.save()
             print("Anotação salva com sucesso.")
         } catch let error as NSError {
             print("Erro ao salvar anotação: \(error.description)")
         }
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    fileprivate func atualizar() {
+        
+        anotacao.setValue(texto.text, forKey: "texto")
+        anotacao.setValue(Date(), forKey: "data")
+        
+        do {
+            try gerenciadorContexto.save()
+            print("Anotação atualizada com sucesso.")
+        } catch let error as NSError {
+            print("Erro ao atualizar anotação: \(error.description)")
+        }
     }
-
-
 }
 
--- Notas Diarias/Notas Diarias/ListaAnotacaoTableViewController.swift
+++ Notas Diarias/Notas Diarias/ListaAnotacaoTableViewController.swift
@@ -54,51 +54,24 @@ class ListaAnotacaoTableViewController: UITableViewController {
         cell.detailTextLabel?.text = data
         return cell
     }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // remove a seleção da célula
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        let anotacaoSelecionada = anotacoes[indexPath.row] as? NSManagedObject
+        performSegue(withIdentifier: "verAnotacao", sender: anotacaoSelecionada )
     }
-    */
 
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "verAnotacao" {
+            let viewControllerDestino = segue.destination as! AnotacaoViewController
+            viewControllerDestino.anotacao = sender as? NSManagedObject
+        }
     }
-    */
     
     func configurarContexto() {
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
