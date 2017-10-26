31c9a49ca3cfb9d1a8094ddc80f0fa6c87e40503
Ronan Luiz Araujo Eustaquio <ronan@ciandt.com>
2017-10-26 19:21:59 -0200

    #App Snapchat Clone - Snaps e Foto
    - Implementação de carregamento das fotos no FirebaseStorage
    - Geração de ids únicos para as imagens
    - Código para habilitar/desabilitar o botão
    - Recuperação da url da imagem enviada para o Firebase

--- Snapchat Clone/Snapchat Clone/FotoViewController.swift
+++ Snapchat Clone/Snapchat Clone/FotoViewController.swift
@@ -7,18 +7,23 @@
 //
 
 import UIKit
+import FirebaseStorage
 
 class FotoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    let qualidadeImagem = CGFloat(0.5) // 50%
+    let idImagem = NSUUID().uuidString
     var imagePicker = UIImagePickerController()
     
     @IBOutlet weak var fotoImageView: UIImageView!
     @IBOutlet weak var descricaoText: UITextField!
+    @IBOutlet weak var proximoButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         imagePicker.delegate = self
+        desabilitarProximo()
     }
     
     @IBAction func selecionarFoto(_ sender: Any) {
@@ -27,13 +32,56 @@ class FotoViewController: UIViewController, UIImagePickerControllerDelegate, UIN
         present(imagePicker, animated: true, completion: nil)
     }
     
+    @IBAction func executarProximo(_ sender: UIButton) {
+        habilitarCarregando()
+        let armazenamento = Storage.storage().reference()
+        let imagensPasta = armazenamento.child("imagens")
+        
+        // Recupera a imagem 
+        if let imagemSelecionada = fotoImageView.image,
+           let imagemDados = UIImageJPEGRepresentation(imagemSelecionada, qualidadeImagem) {
+            imagensPasta.child("\(idImagem).jpg").putData(imagemDados, metadata: nil, completion: { (metaDados, erro) in
+                if erro == nil {
+                    print(metaDados?.downloadURL()?.absoluteURL)
+                    
+                } else {
+                    print("Erro ao enviar a imagem")
+                }
+                
+                self.desabilitarCarregando()
+            })
+        }
+        
+    }
+    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         
         if let imagemRecuperada = info[UIImagePickerControllerOriginalImage] as? UIImage {
             fotoImageView.image = imagemRecuperada
+            habilitarProximo()
         }
         
         imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    fileprivate func habilitarCarregando() {
+        proximoButton.isEnabled = false
+        proximoButton.setTitle("Carregando...", for: .normal)
+    }
+    
+    fileprivate func desabilitarCarregando() {
+        proximoButton.isEnabled = true
+        proximoButton.setTitle("Próximo", for: .normal)
+    }
+    
+    fileprivate func habilitarProximo() {
+        proximoButton.isEnabled = true
+        proximoButton.backgroundColor = UIColor(red: 0.553, green: 0.369, blue: 0.749, alpha: 1)
+    }
+    
+    fileprivate func desabilitarProximo() {
+        proximoButton.isEnabled = false
+        proximoButton.backgroundColor = UIColor.gray
     }
     
 }
