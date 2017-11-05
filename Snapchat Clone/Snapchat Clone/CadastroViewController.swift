6404759ef38677c488f273e26237119e41c907dc
Ronan Luiz Araujo Eustaquio <ronan@ciandt.com>
2017-11-05 12:02:13 -0200

    #App Snapchat Clone - Salvando Usuário
    - Salvando/associando dados do usuário no firebase ao criar uma conta (também no firebase)

--- Snapchat Clone/Snapchat Clone/CadastroViewController.swift
+++ Snapchat Clone/Snapchat Clone/CadastroViewController.swift
@@ -8,12 +8,14 @@
 
 import UIKit
 import FirebaseAuth
+import FirebaseDatabase
 
 class CadastroViewController: BaseViewController {
 
     @IBOutlet weak var email: UITextField!
     @IBOutlet weak var senha: UITextField!
     @IBOutlet weak var senhaConfirmacao: UITextField!
+    @IBOutlet weak var nomeCompletoText: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,27 +31,47 @@ class CadastroViewController: BaseViewController {
     @IBAction func criarConta(_ sender: Any) {
         
         if let email = email.text,
-           let senha = senha.text,
-           let senhaConfirmacao = senhaConfirmacao.text {
+            let senha = senha.text,
+            let senhaConfirmacao = senhaConfirmacao.text,
+            let nomeCompleto = nomeCompletoText.text {
             
-            if senha == senhaConfirmacao {
-                let autenticacao = Auth.auth()
-                autenticacao.createUser(withEmail: email, password: senha, completion: { (usuario, erro) in
-                    if erro == nil {
-                            self.performSegue(withIdentifier: "cadastroLoginSegue", sender: nil)
-                    } else {
-                        let erroRecuperado = erro! as NSError
-                        if let nomeErro = erroRecuperado.userInfo["error_name"]{
-                            let codigoErro = CodigoErroAutenticacao(rawValue: nomeErro as? String ?? "")
-                            let descricaoErro = String(format: "%@.", codigoErro?.descricao ?? "")
-                            self.exibirMensagem(titulo: "Dados incorretos", mensagem: descricaoErro)
-                        }
-                    }
-                })
+            if nomeCompleto.characters.count > 0 {
                 
+                if senha == senhaConfirmacao {
+                    let autenticacao = Auth.auth()
+                    autenticacao.createUser(withEmail: email, password: senha, completion: { (usuario, erro) in
+                        if erro == nil {
+                            if usuario == nil {
+                                self.exibirMensagem(titulo: "Erro de Autenticação", mensagem: "Problemas na autenticação. Tente novamente mais tarde.")
+                                
+                            } else {
+                                // Cria usuário no banco de dados
+                                let database = Database.database().reference()
+                                let usuarios = database.child("usuarios")
+                                
+                                let usuarioDados = ["nome": nomeCompleto, "email": email]
+                                
+                                usuarios.child(usuario!.uid).setValue(usuarioDados)
+                                
+                                self.performSegue(withIdentifier: "cadastroLoginSegue", sender: nil)
+                            }
+                        } else {
+                            let erroRecuperado = erro! as NSError
+                            if let nomeErro = erroRecuperado.userInfo["error_name"]{
+                                let codigoErro = CodigoErroAutenticacao(rawValue: nomeErro as? String ?? "")
+                                let descricaoErro = String(format: "%@.", codigoErro?.descricao ?? "")
+                                self.exibirMensagem(titulo: "Dados incorretos", mensagem: descricaoErro)
+                            }
+                        }
+                    })
+                    
+                } else {
+                    exibirMensagem(titulo: "Dados incorretos", mensagem: "A senha e sua confirmação não conferem.")
+                }
             } else {
-                exibirMensagem(titulo: "Dados incorretos", mensagem: "A senha e sua confirmação não conferem.")
+                exibirMensagem(titulo: "Dados incorretos", mensagem: "Informe o nome completo para cadastro.")
             }
+            
         }
     }
 }
