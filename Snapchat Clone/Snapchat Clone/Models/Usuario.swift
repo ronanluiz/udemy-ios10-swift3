e5037e1137d55d2c0b5d9db4049dca92ca544673
Ronan Luiz Araujo Eustaquio <ronan@ciandt.com>
2017-11-06 18:51:24 -0200

    #App Snapchat Clone - Listando usuários
    - Listando os usuários cadastrados no firebase para envio de snap

---
+++ Snapchat Clone/Snapchat Clone/Models/Usuario.swift
@@ -0,0 +1,9 @@
+//
+//  Usuario.swift
+//  Snapchat Clone
+//
+//  Created by Ronan Luiz Araujo Eustaquio on 05/11/17.
+//  Copyright © 2017 Rlz System. All rights reserved.
+//
+
+import Foundation
